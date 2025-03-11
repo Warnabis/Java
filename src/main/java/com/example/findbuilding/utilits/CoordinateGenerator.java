6b4477d9733117bfdbcfb5eae8f845dc8b87f9ef
Trifonko V. <vtrifonko@gmail.com>
2025-03-11 12:38:14 +0300

    Update CoordinateGenerator.java

--- src/main/java/com/example/findbuilding/utilits/CoordinateGenerator.java
+++ src/main/java/com/example/findbuilding/utilits/CoordinateGenerator.java
@@ -3,8 +3,11 @@ package com.example.findbuilding.utilits;
 import java.util.Random;
 
 public class CoordinateGenerator {
-
     private static final Random random = new Random();
+
+    private CoordinateGenerator() {
+        throw new UnsupportedOperationException("Utility class");
+    }
 
     public static Double generateCoordinateX() {
         return -180 + (180 - (-180)) * random.nextDouble();
